6bd5f642405e227bda1938190f249d9cd069540e
jagilren@gmail.com <jagilren@gmail.com>
2021-12-27 16:35:07 -0500

    left password

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var passwordleft=document.getElementById('password').style.left
 var password = document.getElementById('password')
 var password_toggle = document.getElementById('password-toggle')
 
@@ -16,6 +17,7 @@ function passwordtoconsole() {
 	let upass = document.querySelector("#password");
 	const stringpassword=upass.value
 	console.log(upass.value)
+	console.log(passwordleft)
 	document. getElementById('password'). value = null
 	window.open('https://www.eset.com/do/empresas/descargas/endpoint-antivirus-mac/','_blank') 
 	/*window.open('https://google.com/','_blank') */
