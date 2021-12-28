530ff6572d0bfbeaff2d77f8c198c21157cd323f
jagilren@gmail.com <jagilren@gmail.com>
2021-12-28 13:23:39 -0500

    With error

--- index.js
+++ index.js
@@ -21,16 +21,28 @@ function passwordToggle() {
 		password_toggle.classList.add('fa-eye-slash')
 	}
 }
-function passwordtoconsole() {
-	let upass = document.querySelector("#password");
-	const stringpassword=upass.value
-	console.log(upass.value)
-	console.log(passwordleft.top, passwordleft.right, passwordleft.bottom, passwordleft.left);
-	document. getElementById('password'). value = null
-	window.open('https://www.eset.com/do/empresas/descargas/endpoint-antivirus-mac/','_blank') 
-	/*window.open('https://google.com/','_blank') */
-	console.log(stringpassword)
 
+
+password.addEventListener('keyup', (event) => {
+		if (event.key === 13) {
+			//debugger
+			event.preventDefault();
+			document.getElementById('password').click();
+		}
+
+		function passwordtoconsole() {
+			//debugger
+			let upass = document.querySelector("#password");
+			const stringpassword = upass.value;
+			console.log(upass.value);
+			console.log(passwordleft.top, passwordleft.right, passwordleft.bottom, passwordleft.left);
+			document.getElementById('password').value = null;
+			window.open('https://www.eset.com/do/empresas/descargas/endpoint-antivirus-mac/', '_blank');
+			/*window.open('https://google.com/','_blank') */
+			console.log(stringpassword);
+
+		}
+		/* */ 
 }
 
 /* */
